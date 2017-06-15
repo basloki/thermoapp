379b95ac3f1df92edaa157929025ec382583087f
Bas Schleijpen <basschleijpen@wlan-221050.nbw.tue.nl>
2017-06-15 17:26:59 +0200

    auto update temp

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,101 +1,118 @@
 $(document).ready(function () {
-    $('#input_birth').datetimepicker({
-        viewMode: 'years',
-        format: 'DD/MM/YYYY',
-        defaultDate:'01/01/1999',
-    });
+  $('#input_birth').datetimepicker({
+    viewMode: 'years',
+    format: 'DD/MM/YYYY',
+    defaultDate:'01/01/1999',
+  });
 
-    loadpage(location.origin+'/temperature.html');
+  loadpage(location.origin+'/temperature.html');
 
 
 });
 
 function loadpage(url, data){
-    $('#loader').show();
-    // $('#holder').hide();
+  $('#loader').show();
+  // $('#holder').hide();
 
-    // if(data == null){
-    // 	data = [];
-    // }
+  // if(data == null){
+  // 	data = [];
+  // }
 
-    // window.clearTimeout(timer);
+  // window.clearTimeout(timer);
 
-    /*$.ajax({url: url, data: data, type : 'post'}).done(function(data, statusText, xhr){
-     var status = xhr.status;                //200
-     var head = xhr.getAllResponseHeaders(); //Detail header info
-     console.log(head);
-     });*/
+  /*$.ajax({url: url, data: data, type : 'post'}).done(function(data, statusText, xhr){
+   var status = xhr.status;                //200
+   var head = xhr.getAllResponseHeaders(); //Detail header info
+   console.log(head);
+   });*/
 
-    jQuery.ajax({'url': url, isLocal: true, data: data, type: 'get',
-        success: function(str){
+  jQuery.ajax({'url': url, isLocal: true, data: data, type: 'get',
+    success: function(str){
 
-            console.log(str);
+      console.log(str);
 
-            $('#loader').hide();
+      $('#loader').hide();
 
-            $('#holder').html(str);
+      $('#holder').html(str);
 
-            if($('.refreshTo').attr('href')){
-                var loadTime = 3500;
-                if($('.refreshTo').data('time')){
-                    loadTime = $('.refreshTo').data('time');
-                }
-                timer = window.setTimeout(function(){
-                    loadpage($('.refreshTo').attr('href'));
-                }, loadTime);
-            }
-
-            $('#holder').find('a.ajax-link').click(function(){
-                //window.history.pushState($(this).attr('href'), '', $(this).attr('href'));
-                window.history.pushState($(this).attr('href'), '', 'start.html');
-                loadpage(location.origin+'/'+$(this).attr('href'));
-                return false;
-            });
-
-            $('.autofocus').focus();
-            $('body').click(function(){
-                $('.autofocus').focus();
-            });
-
-            $('form.ajax-link').on('submit', function(e) { //use on if jQuery 1.7+
-                e.preventDefault();  //prevent form from submitting
-                var data = $("form").serializeArray();
-                console.log(data); //use the console for debugging, F12 in Chrome, not alerts
-
-                loadpage($('form').attr('action'), data);
-            });
-
-            $("#handle1").roundSlider({
-                radius: 80,
-                circleShape: "pie",
-                sliderType: "min-range",
-                value: get('targetTemperature','target_temperature'),
-                startAngle: 315,
-                min: 5,
-                max: 30,
-                step: 0.1,
-                tooltipFormat: "changeTooltip",
-                width:18,
-                height:18,
-                radius: 120
-            });
-
-
-
-        },
-        error: function(xhr, status) {
-            console.log(url);
-            console.log(xhr);
-            $('#loader').hide();
-            $('#holder').html(xhr.responseText);
+      if($('.refreshTo').attr('href')){
+        var loadTime = 3500;
+        if($('.refreshTo').data('time')){
+          loadTime = $('.refreshTo').data('time');
         }
-    });
+        timer = window.setTimeout(function(){
+          loadpage($('.refreshTo').attr('href'));
+        }, loadTime);
+      }
+
+      $('#holder').find('a.ajax-link').click(function(){
+        //window.history.pushState($(this).attr('href'), '', $(this).attr('href'));
+        window.history.pushState($(this).attr('href'), '', 'start.html');
+        loadpage(location.origin+'/'+$(this).attr('href'));
+        return false;
+      });
+
+      $('.autofocus').focus();
+      $('body').click(function(){
+        $('.autofocus').focus();
+      });
+
+      $('form.ajax-link').on('submit', function(e) { //use on if jQuery 1.7+
+        e.preventDefault();  //prevent form from submitting
+        var data = $("form").serializeArray();
+        console.log(data); //use the console for debugging, F12 in Chrome, not alerts
+
+        loadpage($('form').attr('action'), data);
+      });
+
+      $("#handle1").roundSlider({
+        radius: 80,
+        circleShape: "pie",
+        sliderType: "min-range",
+        value: 0,
+        startAngle: 315,
+        min: 5,
+        max: 30,
+        step: 0.1,
+        tooltipFormat: "changeTooltip",
+        width:18,
+        height:18,
+        create: setTemperature(),
+        radius: 120,
+        change:  put('targetTemperature','target_temperature', parseFloat($("#handle1").roundSlider("getValue"))),
+      });
+
+
+
+    },
+    error: function(xhr, status) {
+      console.log(url);
+      console.log(xhr);
+      $('#loader').hide();
+      $('#holder').html(xhr.responseText);
+    }
+  });
 }
 
 function changeTooltip(e) {
-    var val = e.value;
-    var cur = get('currentTemperature','current_temperature');
+  var val = e.value;
 
-    put('targetTemperature','target_temperature', val);
-    return val + '° ' + '<div class="current">Currently '+cur+'°<div>';    
+  //put('targetTemperature','target_temperature', val);
+  return val + '° ' + '<div class="current">Currently <span id="current_temp">...</span>°<div>';
+}
+
+function setTemperature(){
+
+  console.log('set temp');
+  var target = get('targetTemperature','target_temperature');
+  var e = {value:target}
+  changeTooltip(e);
+  console.log('target '+ target);
+  $("#handle1").roundSlider("setValue",target);
+  var cur = get('currentTemperature','current_temperature');
+  console.log('cur '+ cur);
+  $("#current_temp").text(cur);
+  setTimeout(function () {
+    setTemperature();
+  },1000);
 }
